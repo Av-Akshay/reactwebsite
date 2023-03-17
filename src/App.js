7bfa3672341123beee41d19bf429bf26036c8b93
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-03-17 07:22:25 -0700

    setup is done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+}
+
+export default App;
